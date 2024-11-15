e6e24fd0ee443d285c092d3af3621ff5ad2939c3
Yoel Mebrahtu <yo3ls2000@gmail.com>
2024-11-15 20:01:26 +0100

    Init

---
+++ src/webparts/dynamicContentWebPart/components/IDynamicContentWebPartProps.ts
@@ -0,0 +1,7 @@
+export interface IDynamicContentWebPartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
